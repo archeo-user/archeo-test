718f5e9eb5cfdea45df98c91fcf6e5837275cd2b
archeo-user <orlando.claudio.1@spes.uniud.it>
2026-09-22 14:38:05 +0200

    Aggiornata la logica di calcolo nel file main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main():
     print(f"Cantiere attivo: {cantiere}")
     print(f"Totale US registrate: {len(unita_presenti)}")
     print("=== modifica di prova ===")
-
+    print("=== modifica di prova2, secondo giorno ===")
+    
 if __name__ == "__main__":
     main()
